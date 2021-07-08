22649803faebb94cd41c0f46bd23413d3e2d9324
Leela Narasimha <leela.narsimha@gmail.com>
2021-07-08 23:40:33 +0530

    Video 4 observable details

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,49 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
+import { from, fromEvent } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
   title = 'rxjs-basics';
+
+  postsArray = [
+    { title: 'Leela1', description: 'Leela1 description1' },
+    { title: 'Leela2', description: 'Leela1 description2' },
+    { title: 'Leela3', description: 'Leela1 description3' },
+  ];
+
+  postsArrayObservable$ = from(this.postsArray);
+
+  promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve('Resolve the promise. sending data');
+    }, 3000);
+  });
+
+  promiseObservable$ = from(this.promise);
+
+  constructor() {
+    this.postsArrayObservable$.subscribe({
+      next: (data) => console.log(data),
+      error: (error) => console.log(error),
+      complete: () => console.log('complete done!'),
+    });
+
+    this.promiseObservable$.subscribe({
+      next: (data) => console.log(data),
+      error: (error) => console.log(error),
+      complete: () => console.log('complete done! promise done'),
+    });
+  }
+
+  ngAfterViewInit() {
+    fromEvent(document.getElementById('click-button')!, 'click').subscribe({
+      next: (data) => console.log(data),
+      error: (error) => console.log(error),
+      complete: () => console.log('complete done! promise done'),
+    });
+  }
 }
